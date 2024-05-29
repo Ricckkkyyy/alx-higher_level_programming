0844da3cfd4b161caa006ccb1be04234978904e7
Ricckkkyyy <ricomainah@gmail.com>
2024-05-29 05:38:22 -0700

    commbo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
+for tens_digit in range(0, 9):
     for ones_digit in range(tens_digit + 1, 10):
         if tens_digit == 8:
-            print(f"{tens_digit}{ones_digit}")
+            print("{}{}".format(tens_digit, ones_digit))
         else:
-            print(f"{tens_digit}{ones_digit}", end=", ")
+            print("{}{}".format(tens_digit,ones_digit), end=", ")
