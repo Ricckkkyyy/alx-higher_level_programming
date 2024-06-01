78b160dd2a9fbb044e5a2f40bb96d7b787bd2cfe
Ricckkkyyy <ricomainah@gmail.com>
2024-06-01 00:53:05 -0700

    reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-def reverse():
-    for i in range (122,98,-1):
-        print(chr(i) if  i % 2 == 0 else  chr(i-32), end='')
-reverse()
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        n = chr(i)
+    else:
+        n = chr(i-32)
+    print("{}".format(n), end="")
