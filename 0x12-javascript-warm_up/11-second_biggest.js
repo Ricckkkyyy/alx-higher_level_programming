62951217c36d322b682f1c9aa3cd93967b6f95af
Ricckkkyyy <ricomainah@gmail.com>
2024-06-20 06:49:34 -0700

    secondbiggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const args = process.argv.slice(2).map(Number);
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
 }
