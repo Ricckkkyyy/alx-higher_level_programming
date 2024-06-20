113194851091a02593c51e4d9939a5c25f050ea8
Ricckkkyyy <ricomainah@gmail.com>
2024-06-20 03:42:22 -0700

    multi

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2]);
-if (!NaN(x)) {
-  for (let i = 0; i < x; i++){
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
   console.log('C is fun');
   }
 }
