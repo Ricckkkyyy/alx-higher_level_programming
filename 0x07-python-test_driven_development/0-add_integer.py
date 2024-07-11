8c6c44ab1dac709bc8e63091d3f70f0380949ad7
Ricckkkyyy <ricomainah@gmail.com>
2024-07-11 03:54:43 -0700

    add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-def add_integer(a, b=98)
-    """
-    Adds two integers or floats after converting them to integers.
-    Parameters:
-        a (int, float): The first number to add. Must be an integer or float.
-        b (int, float, optional): The second number to add. Must be an integer or float. Defaults to 98.
-    Returns:
-        int: The sum of a and b after casting them to integers.
+"""Defines an integer addition function."""
+
+
+def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
     Raises:
-        TypeError: If either a or b is not an integer or float.
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance (a, (int, float)):
-        raise TypeError("a must be an interger or a float")
-    if not isinstance (b, (int, float)):
-        raise TypeError("b must be an interger or a float")
-    return int(a) + int(b)
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
